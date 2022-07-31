b6269b9c6c934200fb83b8061834cf956a4980f2
hirom <1870157+hirom@users.noreply.github.com>
2022-07-31 23:28:39 +0900

    Style Memolist Screen

---
+++ __App copy.jsx
@@ -0,0 +1,27 @@
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+import Hello from "./src/components/Hello";
+
+export default function App() {
+	return (
+		<View style={styles.container}>
+			<Hello bang>World</Hello>
+			<Hello bang style={{ fontSize: 16 }}>
+				Small World
+			</Hello>
+			<Text>Open up App.js to start working on your app!</Text>
+			<StatusBar style="auto" />
+		</View>
+	);
+}
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		backgroundColor: "#fff",
+		alignItems: "center",
+		justifyContent: "center",
+	},
+});
